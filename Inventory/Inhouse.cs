bdf4b01a2d86b260c5214c0ac1c3e71d6cd3e767
seeker1911 <michaelmead007@icloud.com>
2019-12-30 10:55:08 -0600

    add in house parts class

---
+++ Inventory/Inhouse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace Inventory
+{
+    public class Inhouse : Part
+    {
+        // Fields
+        private int _machineID;
+        // Properties
+        public int MachineID
+        {
+            get { return _machineID; }
+            set { _machineID = value; }
+        }
+
+        // Constructor
+
+        public Inhouse(int partID, string name, decimal price, int inStock, int min, int max, int machineID)
+        {
+            _partID = partID;
+            _name = name;
+            _price = price;
+            _inStock = inStock;
+            _min = min;
+            _max = max;
+            _machineID = machineID;
+        }     
+    }
+}
