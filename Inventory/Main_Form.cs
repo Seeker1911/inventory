a587002b14e400f9451f38ceb5ec78c099a3d4d0
seeker1911 <michaelmead007@icloud.com>
2020-01-15 11:26:27 -0600

    update modifyProduct call to remove Form

--- Inventory/Main_Form.cs
+++ Inventory/Main_Form.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -167,12 +167,12 @@ namespace Inventory
             if (MainParts_GridView.CurrentRow.DataBoundItem.GetType() == typeof(Inhouse))
             {
                 Inhouse inhousePart = (Inhouse)MainParts_GridView.CurrentRow.DataBoundItem;
-                new ModifyPart(inhousePart).ShowDialog();
+                new ModifyPartForm(inhousePart).ShowDialog();
             }
             else if (MainParts_GridView.CurrentRow.DataBoundItem.GetType() == typeof(Outsourced))
             {
                 Outsourced outsourcedPart = (Outsourced)MainParts_GridView.CurrentRow.DataBoundItem;
-                new ModifyPart(outsourcedPart).ShowDialog();
+                new ModifyPartForm(outsourcedPart).ShowDialog();
             }
 
         }
@@ -188,7 +188,7 @@ namespace Inventory
                     // Inventory.DeletePart(part);
                     var rowIndex = MainParts_GridView.CurrentCell.RowIndex;
                     MainParts_GridView.Rows.RemoveAt(rowIndex);
-                    Product.RemoveAssociatedPart(rowIndex);
+                    product.RemoveAssociatedPart(rowIndex);
                 }
                 else return;
             }
@@ -263,14 +263,14 @@ namespace Inventory
         private void Main_Products_Add_Btn_Click(object sender, EventArgs e)
         {
             // bring up instance of Add product screen
-            new AddProduct().ShowDialog();
+            new AddProductForm().ShowDialog();
         }
 
         private void Main_Products_Modify_Btn_Click(object sender, EventArgs e)
         {
             // bring up instance of Modfy product screen and product
             Product product = (Product)MainProducts_GridView.CurrentRow.DataBoundItem;
-            new ModifyProduct(product).ShowDialog();
+            new ModifyProductForm(product).ShowDialog();
         }
 
         private void Main_Products_Delete_Btn_Click(object sender, EventArgs e)
@@ -279,7 +279,7 @@ namespace Inventory
             if (confirm == DialogResult.OK)
             {
                 Product product = (Product)MainProducts_GridView.CurrentRow.DataBoundItem;
-                if (Product.AssociatedParts.Count > 0)
+                if (product.AssociatedParts.Count > 0)
                 {
                     MessageBox.Show("Cannot delete a product that has associated parts.  Remove assosicated parts prior to attempting to remove a product.");
                 }
